2ce5ca83da17c712ed6cd41895117ceb00ae5112
jeandgardany <jeandgardany@hotmail.com>
2020-05-09 10:20:30 -0300

    Correções de CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,11 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require jquery
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
 //= require_tree .
 //import 'bootstrap/dist/js/bootstrap';
